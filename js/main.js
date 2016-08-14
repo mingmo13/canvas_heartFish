4305ead819e2f6e9e3fc4b5c20a334b6968672cc
Raina <764883754@qq.com>
2016-08-14 13:48:51 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -11,6 +11,22 @@ var mom;
 var mx;
 var my;
 
+var babyTail=[];
+var babyEye=[];
+var babyBody=[];
+
+var momTail=[];
+var momEye=[];
+var momBodyOra=[];
+var momBodyBlue=[];
+
+var data;
+var wave;
+var helo;
+
+var dust;
+var dustPic=[];
+
 var canWidth;
 var canHeight;
 
@@ -35,6 +51,9 @@ function init(){
 	canWidth=can1.width;
 	canHeight=can1.height;
 
+	ctx1.font='30px Verdana';
+	ctx1.textAlign='center'
+
 	can1.addEventListener('mousemove',onmousemove,false);
 
 	bgPic.src='./src/background.jpg';
@@ -52,6 +71,58 @@ function init(){
 
 	baby=new babyObj();
 	baby.init();
+	//小鱼尾巴
+	for (var i = 0; i < 8; i++) {
+		babyTail[i]=new Image();
+		babyTail[i].src='./src/babyTail'+i+'.png';
+	}
+	//小鱼眼睛
+	for (var i = 0; i < 2; i++) {
+		babyEye[i]=new Image();
+		babyEye[i].src='./src/babyEye'+i+'.png';
+	}
+	//小鱼身体
+	for (var i = 0; i < 20; i++) {
+		babyBody[i]=new Image();
+		babyBody[i].src='./src/babyFade'+i+'.png';
+	}
+
+	//大鱼尾巴
+	for (var i = 0; i < 8; i++) {
+		momTail[i]=new Image();
+		momTail[i].src='./src/bigTail'+i+'.png';
+	}
+
+	for (var i = 0; i < 2; i++) {
+		momEye[i]=new Image();
+		momEye[i].src='./src/bigEye'+i+'.png';
+	}
+
+	//大鱼身体
+	for (var i = 0; i < 8; i++) {
+		momBodyOra[i]=new Image();
+		momBodyBlue[i]=new Image();
+
+		momBodyOra[i].src='./src/bigSwim'+i+'.png';
+		momBodyBlue[i].src='./src/bigSwimBlue'+i+'.png';
+	}
+	//dust
+	for (var i = 0; i < 7; i++) {
+		dustPic[i]=new Image();
+		dustPic[i].src='./src/dust'+i+'.png';
+	}
+
+	data=new dataObj();
+
+	wave=new waveObj();
+	wave.init();
+
+	helo=new heloObj();
+	helo.init();
+
+	dust=new dustObj();
+	dust.init();
+	
 }
 function gameloop(){
 	window.requestAnimFrame(gameloop);
@@ -69,16 +140,25 @@ function gameloop(){
 
 	ctx1.clearRect(0,0,canWidth,canHeight);
 	mom.draw();
-	momFruitCollision();
 	baby.draw();
+	momFruitCollision();
+	momBabyCollision();
+
+	data.draw();
+	wave.draw();
+	helo.draw();
+	dust.draw();
 }
 
 function onmousemove(e) {
-	if (e.offSetX||e.layerX) {
-		mx=e.offSetX==undefined?e.layerX:e.offSetX;
-		my=e.offSetY==undefined?e.layerY:e.offSetY;
+	if (!data.gameOver) {
+		if (e.offSetX||e.layerX) {
+			mx=e.offSetX==undefined?e.layerX:e.offSetX;
+			my=e.offSetY==undefined?e.layerY:e.offSetY;
 
+		}
 	}
+	
 }
 
 
